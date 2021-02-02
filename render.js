b527680c1416aa421eb0f90f63b95b275764a269
Raouf Baghejari <r.baghjeri@chakavak.info>
2021-02-02 13:30:30 +0330

    change image reloading

--- render.js
+++ render.js
@@ -1,4 +1,4 @@
-const {ipcRenderer }=require('electron')
+const { ipcRenderer } = require('electron')
 const $ = require('jquery')
 const btn = document.getElementById('Generator')
 const toast = require('toastr')
@@ -6,36 +6,52 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const url = 'https://fa.wikipedia.org/wiki/Special:Random';
-
 Attr();
 
 
-btn.addEventListener('click',()=>{
-    
+btn.addEventListener('click', () => {
 
-toast.success("Please Wait For Receive ","Loading ...",{timeOut:2000});
 
-    
-     axios(url)
-     .then(response => {
-       const html = response.data;
-       
-       const $ = cheerio.load(html)
-       const statsTable = $('.firstHeading').text();
-       const Body = $('.mw-parser-output p').text();
-       var imgsrc=$('.thumbinner img ').attr('src');
-       imgsrc = "https:"+imgsrc
-       console.log(statsTable)
-       console.log(imgsrc)
-       document.getElementById("Main-Text").innerHTML=Body
-       document.getElementById("Main-Image").src=imgsrc
-        
-       
+        toast.success("Please Wait For Receive ", "Loading ...", { timeOut: 2000 });
 
-       document.getElementById('Header-Text').innerHTML=statsTable
 
-     })
-  }
+        axios(url)
+            .then(response => {
+                const html = response.data;
+                document.getElementById("learn").href = response.request.responseURL;
+                const $ = cheerio.load(html)
+                const statsTable = $('.firstHeading').text();
+                const Body = $('.mw-parser-output p').text();
+                var imgsrc = $('.thumbinner img ').attr('src');
+                document.getElementById("Main-Image").src = imgsrc;
+                if (imgsrc == null) {
+                    const title = response.request.responseURL;
+                    const url = title.match(/([^\/]*)\/*$/)[1];
+                    const ur = "https://fa.wikipedia.org/w/api.php?action=query&prop=pageimages&format=json&piprop=original&titles=" + url;
+
+                    axios(ur).then(r => {
+                        json = r.data['query']['pages'];
+                        Object.keys(json).forEach(function(key) {
+                            imgsrc = json[key]['original']['source'];
+                            document.getElementById("Main-Image").src = imgsrc;
+
+
+                        })
+                    })
+                } else {
+                    imgsrc = "https:" + imgsrc;
+                    document.getElementById("Main-Image").src = imgsrc;
+                }
+
+                document.getElementById("Main-Text").innerHTML = Body;
+
+
+
+
+                document.getElementById('Header-Text').innerHTML = statsTable
+
+            })
+    }
 
 
 
@@ -44,14 +60,14 @@ toast.success("Please Wait For Receive ","Loading ...",{timeOut:2000});
 
 
 
-function Attr(){
-  toast.options.closeButton=true;
-toast.options.showMethod='slideDown';
-toast.options.hideMethod='slideUp';
-toast.options.closeMethod='slideUp';
-toast.options.progressBar=true
+function Attr() {
+    toast.options.closeButton = true;
+    toast.options.showMethod = 'slideDown';
+    toast.options.hideMethod = 'slideUp';
+    toast.options.closeMethod = 'slideUp';
+    toast.options.progressBar = true
 }
 
-function Error_Image_Load(){
-  document.getElementById("Main-Image").src="https://icon-library.net/images/photo-gallery-icon-vector/photo-gallery-icon-vector-10.jpg"
+function Error_Image_Load() {
+    document.getElementById("Main-Image").src = "https://icon-library.net/images/photo-gallery-icon-vector/photo-gallery-icon-vector-10.jpg"
 }
